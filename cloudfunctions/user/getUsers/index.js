22096167324b0cf862c3d0ccc300a4ec20d97b3a
YunhaoDong <2274006799@qq.com>
2022-12-08 00:02:04 +0800

    user

--- cloudfunctions/user/getUsers/index.js
+++ cloudfunctions/user/getUsers/index.js
@@ -1,5 +1,5 @@
 const cloud = require('wx-server-sdk')
-const isNotStudent=require('../util/permission.js')
+const permission=require('../util/permission.js')
 
 // 云环境初始化
 cloud.init({
@@ -10,23 +10,52 @@ cloud.init({
 const db = cloud.database({
   throwOnNotFound: true
 })
+const _ = db.command
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
-  return await db.collection('User').where({
-    openid:wxContext.OPENID
-  }).get().then(res=>{
+  const pageOffset={
+    limit:event.info.limit?Math.min(event.info.limit,20):10,
+    offset:event.info.offset?event.info.offset:0,
+  }
+  const pageQuery={
+    search:event.info.search?event.info.search:"",
+    permission:event.info.permission?event.info.permission:0,
+  }
+  const permissionValidate = await permission.isSuperAdmin();
+  if(permissionValidate.code!=="success"){
+    return permissionValidate;
+  }
+  console.log(pageQuery)
+  return await db.collection('User').where(_.or([{
+    name: db.RegExp({
+      regexp: '.*' + pageQuery.search,
+      options: 'i',
+    })
+  },
+  {
+    stuid: db.RegExp({
+      regexp: '.*' +  pageQuery.search,
+      options: 'i',
+    })
+  }
+]).and({
+  permission:_.gte(pageQuery.permission)
+})).skip(pageOffset.offset)
+  .limit(pageOffset.limit+1)
+  .get().then(res=>{
+    const hasMore=res.data.length>pageOffset.limit?true:false
     return {
-      code: 'success',
-	    info:res.data,
-      status: 200,
+      code:'success',
+      data:res.data,
+      hasMore:hasMore,
+      status:200
     }
   }).catch(e=>{
     return {
-      code: 'fail',
-      des: e,
-      status: 500,
+      code:'fail',
+      status:500,
+      des:e
     }
   })
 }
