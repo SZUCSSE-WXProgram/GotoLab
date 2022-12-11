0b5500577a0f97c392cdfa603bbd0d3665c2d331
YunhaoDong <2274006799@qq.com>
2022-12-11 20:14:19 +0800

    refactor

--- cloudfunctions/activityType/util/permission.js
+++ cloudfunctions/activityType/util/permission.js
@@ -1,94 +1,94 @@
 const cloud = require('wx-server-sdk')
 
 cloud.init({
-  env: cloud.DYNAMIC_CURRENT_ENV
+    env: cloud.DYNAMIC_CURRENT_ENV
 })
 const db = cloud.database()
 
 // 系统管理员鉴权
 exports.isSuperAdmin = async () => {
-  const wxContext = cloud.getWXContext();
-  return await db.collection('User').where({
-    openid:wxContext.OPENID
-  }).get().then(res=>{
-    if(res.data[0].permission===2){
-      return {
-        code: 'success',
-        status: 200,
-      }
-    }else{
-      return {
-        code: 'fail',
-        des:'权限不足',
-        status: 403,
-      }
-    }
-  }).catch(e=>{
-    return {
-      code: 'fail',
-      des:e,
-      status: 500,
-    }
-  })
+    const wxContext = cloud.getWXContext();
+    return await db.collection('User').where({
+        openid: wxContext.OPENID
+    }).get().then(res => {
+        if (res.data[0].permission === 2) {
+            return {
+                code: 'success',
+                status: 200,
+            }
+        } else {
+            return {
+                code: 'fail',
+                des: '权限不足',
+                status: 403,
+            }
+        }
+    }).catch(e => {
+        return {
+            code: 'fail',
+            des: e,
+            status: 500,
+        }
+    })
 }
 
 // Group管理员鉴权
 exports.isGroupAdmin = async (groupID) => {
-  if(this.isSuperAdmin().code==='success'){
-    return {
-      code: 'success',
-      status: 200,
-    }
-  }
-  const wxContext = cloud.getWXContext();
-  return await db.collection('User').where({
-    openid:wxContext.OPENID
-  }).get().then(res=>{
-    for(let i=0;i<len(res.data[0].groups);i++){
-      if(res.data[0].groups[i]===groupID){
+    if (this.isSuperAdmin().code === 'success') {
         return {
-          code: 'success',
-          status: 200,
+            code: 'success',
+            status: 200,
         }
-      }
     }
-    return {
-      code: 'fail',
-      des:'权限不足',
-      status: 403,
-    }
-  }).catch(e=>{
-    return {
-      code: 'fail',
-      des:e,
-      status: 500,
-    }
-  })
+    const wxContext = cloud.getWXContext();
+    return await db.collection('User').where({
+        openid: wxContext.OPENID
+    }).get().then(res => {
+        for (let i = 0; i < len(res.data[0].groups); i++) {
+            if (res.data[0].groups[i] === groupID) {
+                return {
+                    code: 'success',
+                    status: 200,
+                }
+            }
+        }
+        return {
+            code: 'fail',
+            des: '权限不足',
+            status: 403,
+        }
+    }).catch(e => {
+        return {
+            code: 'fail',
+            des: e,
+            status: 500,
+        }
+    })
 }
 
 // 非学生用户鉴权
 exports.isNotStudent = async () => {
-  const wxContext = cloud.getWXContext();
-  return await db.collection('User').where({
-    openid:wxContext.OPENID
-  }).get().then(res=>{
-    if(res.data[0].permission===1||res.data[0].permission===2){
-      return {
-        code: 'success',
-        status: 200,
-      }
-    }else{
-      return {
-        code: 'fail',
-        des:'权限不足',
-        status: 403,
-      }
-    }
-  }).catch(e=>{
-    return {
-      code: 'fail',
-      des:e,
-      status: 500,
-    }
-  })
+    const wxContext = cloud.getWXContext();
+    return await db.collection('User').where({
+        openid: wxContext.OPENID
+    }).get().then(res => {
+        if (res.data[0].permission === 1 || res.data[0].permission === 2) {
+            return {
+                code: 'success',
+                status: 200,
+            }
+        } else {
+            return {
+                code: 'fail',
+                des: '权限不足',
+                status: 403,
+            }
+        }
+    }).catch(e => {
+        return {
+            code: 'fail',
+            des: e,
+            status: 500,
+        }
+    })
 }
