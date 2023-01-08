e4ebc6c6af030f5822d90279bd4940a2148feb35
Yunhao <2274006799@qq.com>
2023-01-08 21:24:28 +0800

    update permission

--- cloudfunctions/user/util/permission.js
+++ cloudfunctions/user/util/permission.js
@@ -43,20 +43,20 @@ exports.isGroupAdmin = async (groupID) => {
     }
     const wxContext = cloud.getWXContext();
     return await db.collection('User').where({
-        openid: wxContext.OPENID
+        openid: wxContext.OPENID,
+        groups: groupID
     }).get().then(res => {
-        for (let i = 0; i < res.data[0].groups.length; i++) {
-            if (res.data[0].groups[i] === groupID) {
-                return {
-                    code: 'success',
-                    status: 200,
-                }
+        if (res.data.length > 0) {
+            return {
+                code: 'success',
+                status: 200,
             }
-        }
-        return {
-            code: 'fail',
-            des: '权限不足',
-            status: 403,
+        } else {
+            return {
+                code: 'fail',
+                des: '权限不足',
+                status: 403,
+            }
         }
     }).catch(e => {
         return {
