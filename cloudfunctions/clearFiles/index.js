9208f5ff4b97722622259699ed081b748ffd2d08
Yunhao <2274006799@qq.com>
2023-03-02 01:17:05 +0800

    update trigger

--- cloudfunctions/clearFiles/index.js
+++ cloudfunctions/clearFiles/index.js
@@ -19,21 +19,46 @@ exports.main = async (event, context) => {
     const fileList = await db.collection('Files').where({
         createTime: _.lte(current),
         type: 'exports',
-    }).get().then(res => {
-        return res.data
-    })
-    for (let i = 0; i < fileList.length; i++) {
-        console.log(fileList[i].createTime - new Date().getTime())
-        await cloud.deleteFile({
-            fileList: [fileList[i].url],
+    }).limit(10)
+        .get().then(res => {
+            return res.data
         })
-        await db.collection('Files').doc(fileList[i]._id).remove()
-    }
-    return {
-        code: 'success',
-        des: '清理成功',
-        files: fileList,
-        status: 200,
+    if (fileList.length === 0) {
+        return {
+            code: 'success',
+            des: '无需清理',
+            files: fileList,
+            status: 200,
+        }
     }
 
+    //获取url数组
+    const urls = fileList.map(item => {
+        return item.url
+    })
+    const _ids = fileList.map(item => {
+        return item._id
+    })
+    const tasks = []
+    tasks.push(cloud.deleteFile({
+        fileList: urls,
+    }))
+    tasks.push(db.collection('Files').where({
+        _id: _.in(_ids),
+    }).remove())
+    try {
+        await Promise.all(tasks)
+        return {
+            code: 'success',
+            des: '清理成功',
+            files: fileList,
+            status: 200,
+        }
+    } catch (e) {
+        return {
+            code: 'fail',
+            des: e,
+            status: 500,
+        }
+    }
 };
